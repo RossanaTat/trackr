4e4f829ea589d08caa0e9eacb2ee6b99c63805b1
RossanaTat <rossana.tatulli@studbocconi.it>
2025-04-16 17:08:55 -0400

    initial setup

---
+++ R/zzz.R
@@ -0,0 +1,26 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.sgtrackr <- list(
+    sgtrackr.default_source      = "wdi",
+    sgtrackr.default_targetyear  = 2030,
+    sgtrackr.default_best        = "high",
+    #sgtrackr.default_floor      = 0,
+    #sgtrackr.default_ceiling    = 100,
+    sgtrackr.default_granularity = 0.1,
+    sgtrackr.default_pctl        = FALSE,
+    sgtrackr.default_speed       = TRUE
+    #sgtrackr.default_pctlseq     = seq(20, 80, 20),
+    #sgtrackr.default_speedseq    = c(0.25, 0.5, 1, 2, 4)
+  )
+  toset <- !(names(op.sgtrackr) %in% names(op))
+  if (any(toset)) options(op.sgtrackr[toset])
+  invisible()
+}
+
+# Get options
+## Example
+## source <- get_sgtrackr_opt("default_source")
+
+get_sgtrackr_opt <- function(name) {
+  getOption(paste0("sgtrackr.", name))
+}
