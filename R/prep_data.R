f5b08184dcf94c8dc43ee006d23cecc5301d7111
RossanaTat <rossana.tatulli@studbocconi.it>
2025-06-24 09:44:59 -0400

    refactor prep data

--- R/prep_data.R
+++ R/prep_data.R
@@ -38,40 +38,56 @@ prep_data <- function(indicator      = "EG.ELC.ACCS.ZS",
   dt <- qDT(data)
 
   setnames(dt,
-           old = c(code_col, year_col, indicator),
-           new = c("code", "year", "y"),
+           old         = c(code_col, year_col, indicator),
+           new         = c("code", "year", "y"),
            skip_absent = FALSE)
 
   dt <- dt[year >= startyear_data & year <= endyear_data, .(code, year, y)]
-  setorder(dt, code, year)
+
+  setorder(dt,
+           code,
+           year)
 
   # _______________________________________________________
   # Compute annualized changes from 5 to 10 years ah.  ####
   # _______________________________________________________
 
-  dt[, paste0("c", 5:10) := lapply(5:10, function(n) (shift(y, type = "lead", n = n) - y) / n), by = code]
+
+  dt[, paste0("c", 5:10) := Map(function(n) (shift(y,
+                                                   type = "lead",
+                                                   n = n) - y) / n, 5:10)]
+
 
   # Reshape to long format
-  dt_long <- melt(dt,
-                  id.vars       = c("code", "year", "y"),
-                  measure.vars  = patterns("^c[5-9]$|^c10$"),
-                  variable.name = "duration",
-                  value.name    = "change",
-                  variable.factor = FALSE)
+  dt_long <- melt(
+    dt,
+    id.vars = c("code", "year", "y"),
+    measure.vars = paste0("c", 5:10),
+    variable.name = "duration",
+    value.name = "change"
+  )
+
+  setnames(dt_long,
+           old = c("year", "y"),
+           new = c("year_start", "initialvalue"))
+
+  dt_long[, duration := as.numeric(gsub("^c", "", as.character(duration)))][,
+            year_end := year_start + duration]
+
+
+  setorder(dt_long, code, year_start, duration)
+
+  dt_long <- dt_long[,
+                     .SD[1], by = .(code, year_start)]
 
   dt_long <- dt_long[!is.na(change)]
-  dt_long[, duration := as.numeric(gsub("^c", "", duration))]
-  dt_long[, year_end := year + duration]
-  setnames(dt_long, c("year", "y"), c("year_start", "initialvalue"))
-
-  # Keep only the shortest available spell per (code, year_start)
-  setorder(dt_long, code, year_start, duration)
-  dt_long <- dt_long[, .SD[1], by = .(code, year_start)]
-
-  # Expand rows to balance representation across countries
+  dt_long[, duration := NULL]
   dt_long[, n := .N, by = code]
-  dt_long[, expansion := round(max(n) / n)]
-  dt_long <- dt_long[rep(1L:.N, times = expansion)]
+
+  max_n <- dt_long[, max(n)]
+  dt_long[, expansion := round(max_n / n)]
+
+  dt_long <- dt_long |> splitstackshape::expandRows('expansion')
 
   # _____________________________________________
   # Create folds for cross-validation ####
@@ -79,6 +95,7 @@ prep_data <- function(indicator      = "EG.ELC.ACCS.ZS",
 
   folds <- unique(dt_long[, .(code)])
   folds[, fold_id := sample(1:5, .N, replace = TRUE)]
+
 
   # _____________________________________________
   # Handle min and max ####
@@ -117,3 +134,126 @@ prep_data <- function(indicator      = "EG.ELC.ACCS.ZS",
   ))
 }
 
+###Old version ###################33
+old_prep_data <- function(indicator      = "EG.ELC.ACCS.ZS",
+                      data           = wbstats::wb_data(indicator = indicator, lang = "en", country = "countries_only"),
+                      startyear_data = 2000,
+                      floor          = 0,
+                      ceiling        = 100,
+                      granularity    = 0.1,
+                      code_col       = "iso3c",
+                      year_col       = "date",
+                      verbose = TRUE) {
+
+
+  # ________________________________
+  # Start formatting the data ####
+  # ________________________________
+
+  # Convert to data.table
+  dt <- data.table::as.data.table(data)
+
+  # Standardize column names
+  data.table::setnames(dt,
+                       old         = c(code_col, year_col, indicator),
+                       new         = c("code", "year", "y"),
+                       skip_absent = FALSE)
+
+  dt <- dt |>
+    fselect(code, year, y) |>
+    fsubset(year >= startyear_data)
+
+  data.table::setorder(dt,
+                       code,
+                       year)
+  # _______________________________________________________
+  # Compute annualized changes from 5 to 10 years ah.  ####
+  # _______________________________________________________
+
+  # To optimize with collapse
+
+  dt <- dt |>
+    fmutate(c5   = (lead(y,5)-y)/5,
+            c6  = (lead(y,6)-y)/6,
+            c7  = (lead(y,7)-y)/7,
+            c8  = (lead(y,8)-y)/8,
+            c9  = (lead(y,9)-y)/9,
+            c10 = (lead(y,10)-y)/10)
+
+  # Reshape to long format
+
+  dt <- dt |>
+    pivot(ids              = c("code", "year", "y"),
+          values           = paste0("c", 5:10),
+          names            = list(variable = "duration"),
+          how              = "longer") |>
+    fsubset(!is.na(value)) |>
+    frename("year_start"   = "year",
+            "change"       = "value",
+            "initialvalue" = "y") |>
+    fmutate(duration       = as.numeric(gsub("^c", "", duration)),
+            year_end       = year_start + duration) |>
+    fselect(-duration) |>
+    group_by(code,year_start) |>
+    # TODO move to collapse
+    # The line below imply that we only use 6-year spells if there is no 5-year spell, and only use 7-year spells if there is no 5 and 6-year spell etc.
+    # The advantage of doing so is that we rely more consistently on spells of the same length.
+    slice(1) |> # TODO move to collapse |>
+    ungroup() |>
+
+    # Multiply the rows of countries with little data so they have about as many rows as countries with a lot of data
+    fcount(code, name = "n", add = TRUE) |>
+    #add_count(code) |>
+    fmutate(expansion = round(max(n)/n)) |>
+    splitstackshape::expandRows('expansion')
+
+  # _____________________________________________
+  # Create folds for cross-validation ####
+  # _____________________________________________
+
+  folds <- dt |>
+    fselect(code) |>
+    distinct()
+
+  folds$fold_id <- sample(1:5,
+                          size          = nrow(folds),
+                          replace       = TRUE)
+
+  # ________________________________
+  # Return ####
+  # ________________________________
+
+
+  res_data    <- joyn::joyn(dt,
+                            folds,
+                            by         = "code",
+                            match_type = "m:1",
+                            reportvar  = FALSE,
+                            verbose    = FALSE) |>
+    as.data.table()
+
+  # Calculate floor/ceiling adjusted bounds
+  min_val <- round(
+    ifelse(is.na(floor),
+           min(dt$initialvalue, na.rm = TRUE),
+           floor) / granularity
+  ) * granularity
+
+  max_val <- round(
+    ifelse(is.na(ceiling),
+           max(dt$initialvalue, na.rm = TRUE),
+           ceiling) / granularity
+  ) * granularity
+
+  return(
+    invisible(list(
+      data_model  = res_data,
+      min = min_val,
+      max = max_val,
+      indicator = indicator
+    )))
+
+
+  if (verbose) cli::cli_alert_success("user data successfuly formatted")
+
+}
